5f8b12135905873d207fd24fae81fdd970f7a23f
차승일 <121073208+Lianiscoming@users.noreply.github.com>
2024-05-17 23:37:23 +0900

    Update CorsMvcConfig.java

--- src/main/java/com/back/cinetalk/config/CorsMvcConfig.java
+++ src/main/java/com/back/cinetalk/config/CorsMvcConfig.java
@@ -3,7 +3,7 @@ package com.back.cinetalk.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// cicd test 중
+// cicd test 중 test 1
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
 
