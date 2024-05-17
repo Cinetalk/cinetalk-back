be5e95df7a7e20166e5f7141e55e2228b164a3ea
차승일 <121073208+Lianiscoming@users.noreply.github.com>
2024-05-17 18:50:07 +0900

    Update CinetalkApplication.java

--- src/main/java/com/back/cinetalk/CinetalkApplication.java
+++ src/main/java/com/back/cinetalk/CinetalkApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //
-// cicd 구성 중 test
+// cicd 구성 중 test 1
 public class CinetalkApplication {
 
     public static void main(String[] args) {
