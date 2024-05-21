f239108d68bf1c896a3266a7968eaeba698c08bf
kallis0926 <kallis0926@naver.com>
2024-05-21 15:03:43 +0900

    jar -> war 배포패키지 변경

--- src/main/java/com/back/cinetalk/CinetalkApplication.java
+++ src/main/java/com/back/cinetalk/CinetalkApplication.java
@@ -2,11 +2,17 @@ package com.back.cinetalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 // 뭐가 문제일까
-public class CinetalkApplication {
+public class CinetalkApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(CinetalkApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(CinetalkApplication.class, args);
     }
