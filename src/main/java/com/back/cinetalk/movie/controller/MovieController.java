57a4dba73b89e3f79910d951dd949581637604eb
TAEWOOKK <kto5294@gmail.com>
2024-06-09 17:03:58 +0900

    api 설명 추가및 순서 변경

--- src/main/java/com/back/cinetalk/movie/controller/MovieController.java
+++ src/main/java/com/back/cinetalk/movie/controller/MovieController.java
@@ -4,7 +4,6 @@ import com.back.cinetalk.movie.dto.MovieDetailDTO;
 import com.back.cinetalk.movie.service.MainColorExtract;
 import com.back.cinetalk.movie.service.MovieMainService;
 import com.back.cinetalk.movie.service.MovieDetailService;
-import com.back.cinetalk.review.repository.ReviewRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -30,8 +29,14 @@ public class MovieController {
     private final MovieMainService movieMainService;
     private final MainColorExtract mainColorExtract;
 
+    @GetMapping("/{movie_id}")
+    @Operation(summary = "영화 상세정보 ",description = "영화 상세정보 api")
+    public MovieDetailDTO getMovieDetails(@PathVariable String movie_id) throws IOException {
+        return movieDetailService.getMovieDetail(movie_id);
+    }
+
     @GetMapping("/list")
-    @Operation(summary = "영화 리스트",description = "최신 영화 리스트")
+    @Operation(summary = "주간 최신 영화 리스트",description = "최신 영화 리스트")
     @ApiResponse(responseCode = "200",description = "출력완료",
             content = @Content(schema = @Schema(implementation = ResponseBody.class
             )))
@@ -40,18 +45,8 @@ public class MovieController {
         return movieMainService.nowPlayingList();
     }
 
-    @PostMapping("/test")
-    public Map<String, Object> getSearchlist(String query) throws IOException {
-
-        return movieMainService.getOneByName(query);
-    }
-
-    @GetMapping("/{movie_id}")
-    public MovieDetailDTO getMovieDetails(@PathVariable String movie_id) throws IOException {
-        return movieDetailService.getMovieDetail(movie_id);
-    }
-
     @GetMapping("/ReviewByUser")
+    @Operation(summary = "user의 최근톡",description = "회원의 리뷰 목록 최신순")
     public ResponseEntity<?> ReviewByUser(HttpServletRequest request) throws IOException {
 
         List<Map<String, Object>> maps = movieMainService.ReviewByUser(request);
@@ -60,6 +55,7 @@ public class MovieController {
     }
 
     @GetMapping("/HidingPiece")
+    @Operation(summary = "숨겨진 명작",description = "리뷰가 제일 많이 달린 영화 10개에 대한 정보 출력")
     public ResponseEntity<?> HidingPiece() throws IOException {
 
         List<Map<String, Object>> maps = movieMainService.HidingPiece();
@@ -68,6 +64,7 @@ public class MovieController {
     }
 
     @GetMapping("/MentionKeword")
+    @Operation(summary = "자주 언급되는 키워드",description = "오늘 자 모든 리뷰의 자주 언급된 키워드 상위 5개가 들어간 리뷰 10개씩을 출력")
     public ResponseEntity<?> MentionKeword(){
 
         long startTime = System.currentTimeMillis();
@@ -82,12 +79,14 @@ public class MovieController {
     }
 
     @GetMapping("/TotalReviewCount")
+    @Operation(summary = "톡이 쌓였어요",description = "등록된 모든 리뷰의 개수 출력")
     public ResponseEntity<?> TotalReviewCount(){
 
         return new ResponseEntity<>(movieMainService.TotalReviewCount(),HttpStatus.OK);
     }
 
     @GetMapping("/imagecolor")
+    @Operation(summary = "상세페이지용 이미지 주요색 출력",description = "이미지의 url 입력시 이미지의 주요색 hex코드 반환 api(현재 사용안함)")
     public String imagecolor(@RequestParam(value = "url") String url)throws  Exception{
 
         return mainColorExtract.ColorExtract(url);
