74deae9e3bf10b0cea153dc536da6849a46fae02
TAEWOOKK <kto5294@gmail.com>
2024-06-12 01:12:00 +0900

    CIN-46 자주쓰는 키워드 최적화

---
+++ src/main/java/com/back/cinetalk/movie/dto/MentionKeywordDTO.java
@@ -0,0 +1,18 @@
+package com.back.cinetalk.movie.dto;
+
+import com.back.cinetalk.review.dto.ReviewDTO;
+import com.back.cinetalk.review.entity.QReviewEntity;
+import com.back.cinetalk.review.entity.ReviewEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MentionKeywordDTO {
+    private ReviewEntity reviewinfo;
+    private String nickname;
+}
