331e4be6b426bd2dd690d9ce4664f98c75c915c3
kallis0926 <kallis0926@naver.com>
2024-06-14 02:06:10 +0900

    test 4

--- src/main/java/com/back/cinetalk/CinetalkApplication.java
+++ src/main/java/com/back/cinetalk/CinetalkApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+// @EnableJpaAuditing Spring boot 에서 이 어노테이션을 지원함
 public class CinetalkApplication{
 
     public static void main(String[] args) {
