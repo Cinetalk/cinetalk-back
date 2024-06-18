4b8d7bc433fa7c9ee55407f5a3696f5b7990db9e
차승일 <121073208+Lianiscoming@users.noreply.github.com>
2024-06-18 11:41:58 +0900

    Update ReviewController.java

--- src/main/java/com/back/cinetalk/review/controller/ReviewController.java
+++ src/main/java/com/back/cinetalk/review/controller/ReviewController.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-
+// cicd test
 @RestController
 @RequestMapping("/reviews")
 
