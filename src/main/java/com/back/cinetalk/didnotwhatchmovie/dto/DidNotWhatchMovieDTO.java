f3dc80ea9e3acf542c0bbbc2b804a69d7efa4932
차승일 <121073208+Lianiscoming@users.noreply.github.com>
2024-06-26 13:30:08 +0900

    CIN-78 service 로직 개선

--- src/main/java/com/back/cinetalk/didnotwhatchmovie/dto/DidNotWhatchMovieDTO.java
+++ src/main/java/com/back/cinetalk/didnotwhatchmovie/dto/DidNotWhatchMovieDTO.java
@@ -1,19 +1,13 @@
 package com.back.cinetalk.didnotwhatchmovie.dto;
 
-import com.back.cinetalk.movie.entity.MovieEntity;
-import com.back.cinetalk.user.entity.UserEntity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class DidNotWhatchMovieDTO {
-
-    private UserEntity user;
-    private MovieEntity movie;
+@Data
+public class DidNotWatchMovieDTO {
 
 }
