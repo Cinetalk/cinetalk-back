00d1b7ad9959d62662d26986cb708eaaf6aeb8c1
차승일 <121073208+Lianiscoming@users.noreply.github.com>
2024-06-26 13:27:36 +0900

    CIN-78 service 로직 개션

--- src/main/java/com/back/cinetalk/review/repository/ReviewRepository.java
+++ src/main/java/com/back/cinetalk/review/repository/ReviewRepository.java
@@ -1,18 +1,29 @@
 package com.back.cinetalk.review.repository;
 
+import com.back.cinetalk.genre.entity.GenreEntity;
 import com.back.cinetalk.review.entity.ReviewEntity;
+import com.back.cinetalk.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-@Repository // JpaRepository를 상속 받으면 이 어노테이션을 지원함.
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> , QuerydslPredicateExecutor<ReviewEntity>, ReviewRepositoryCustom {
 
    boolean existsByUserIdAndMovieId(Long userId, Long movieId);
 
    List<ReviewEntity> findTop10ByContentContainingAndParentReviewIsNullOrderByCreatedAtAsc(String Content);
 
-   List<ReviewEntity> findByUserId(Long userId);
+   List<ReviewEntity> findByUserAndParentReviewIsNullOrderByCreatedAt(UserEntity user);
+
+   List<ReviewEntity> findByUserAndParentReviewIsNullOrderByCreatedAtDesc(UserEntity user);
+
+   int countByParentReview(ReviewEntity reviewEntity);
+
+   @Query("SELECT COUNT(rg) FROM ReviewGenreEntity rg JOIN rg.review r WHERE r.user = :user AND rg.genre = :genre")
+   long countByUserAndGenre(@Param("user") UserEntity user, @Param("genre") GenreEntity genre);
+
+   List<ReviewEntity> findByUser_Email(String email);
 }
