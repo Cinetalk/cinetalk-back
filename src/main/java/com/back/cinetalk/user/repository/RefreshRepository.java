3dc2e3bd2d52dfcc35545b4546500645fad7813f
TAEWOOKK <kto5294@gmail.com>
2024-06-29 17:27:50 +0900

    CIN-84 redis 연동중

--- src/main/java/com/back/cinetalk/user/repository/RefreshRepository.java
+++ src/main/java/com/back/cinetalk/user/repository/RefreshRepository.java
@@ -4,12 +4,13 @@ import com.back.cinetalk.user.entity.RefreshEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
+public interface RefreshRepository extends CrudRepository<RefreshEntity, Long> {
 
     Boolean existsByRefresh(String refresh);
 
