37de94736be9ac2342afad7d10379dc8073a52fe
kallis0926 <kallis0926@naver.com>
2024-07-04 13:27:03 +0900

    CIN-88 나와 취향이 같은 사람들

--- src/main/java/com/back/cinetalk/movie/service/MovieMainService.java
+++ src/main/java/com/back/cinetalk/movie/service/MovieMainService.java
@@ -1,9 +1,12 @@
 package com.back.cinetalk.movie.service;
 
+import com.back.cinetalk.badge.entity.BadgeEntity;
+import com.back.cinetalk.badge.entity.QBadgeEntity;
 import com.back.cinetalk.genre.entity.GenreEntity;
 import com.back.cinetalk.genre.entity.QGenreEntity;
 import com.back.cinetalk.movie.dto.HoxyDTO;
 import com.back.cinetalk.movie.dto.MovieDTO;
+import com.back.cinetalk.movie.dto.UserEqDTO;
 import com.back.cinetalk.movie.entity.MovieEntity;
 import com.back.cinetalk.movie.repository.MovieRepository;
 import com.back.cinetalk.review.dto.ReviewDTO;
@@ -12,8 +15,11 @@ import com.back.cinetalk.review.entity.ReviewEntity;
 import com.back.cinetalk.review.repository.ReviewRepository;
 import com.back.cinetalk.reviewGenre.entity.QReviewGenreEntity;
 import com.back.cinetalk.user.MyPage.component.UserByAccess;
+import com.back.cinetalk.user.entity.QUserEntity;
 import com.back.cinetalk.user.entity.UserEntity;
 import com.back.cinetalk.user.jwt.JWTUtil;
+import com.back.cinetalk.userBadge.entity.QUserBadgeEntity;
+import com.back.cinetalk.userBadge.entity.UserBadgeEntity;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
@@ -37,6 +43,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -54,6 +61,7 @@ public class MovieMainService {
     QReviewEntity review = QReviewEntity.reviewEntity;
 
     QReviewGenreEntity reviewGenre = QReviewGenreEntity.reviewGenreEntity;
+    QBadgeEntity badge = QBadgeEntity.badgeEntity;
 
 
     //TODO 최신 영화 받아오기
@@ -298,6 +306,52 @@ public class MovieMainService {
 
         return new ResponseEntity<>(resultList, HttpStatus.OK);
     }
+
+    /*
+    나와 취향이 비슷한 사람들
+    case 1. 내가 장착한 뱃지를 기준으로 작성한 리뷰가 많은 사용자 순
+    casw 2. 리뷰가 많은 사용자
+
+    1. 리뷰를 많이 작성한 유저를 가져오고
+    2. 그 중에서 내가 장착한 뱃지가 있으면 먼저 가져온다
+    * */
+
+    public ResponseEntity<?> UserEqReviewers(HttpServletRequest request) throws IOException {
+        UserEntity userEntity = userByAccess.getUserEntity(request);
+
+        // 유저가 갖고 있는 뱃지 목록 조회
+        List<Long> currentUserBadgeIds = queryFactory.select(QUserBadgeEntity.userBadgeEntity.badge.id)
+                .from(QUserBadgeEntity.userBadgeEntity)
+                .where(QUserBadgeEntity.userBadgeEntity.user.eq(userEntity))
+                .fetch();
+
+        // 리뷰를 많이 작성한 유저 조회
+        List<UserEntity> users = queryFactory.select(QUserEntity.userEntity)
+                .from(QReviewEntity.reviewEntity)
+                .join(QReviewEntity.reviewEntity.user, QUserEntity.userEntity)
+                .groupBy(QUserEntity.userEntity)
+                .orderBy(QReviewEntity.reviewEntity.count().desc())
+                .fetch();
+
+        List<UserEqDTO> resultList = new ArrayList<>();
+
+        for (UserEntity u : users) {
+            List<String> badges = u.getUserBadgeEntityList().stream()
+                    .map(ub -> ub.getBadge().getName())
+                    .collect(Collectors.toList());
+            UserEqDTO userEq = UserEqDTO.builder()
+                    .userId(u.getId())
+                    .userName(u.getNickname())
+                    .email(u.getEmail())
+                    .badges(badges)
+                    .build();
+            resultList.add(userEq);
+        }
+
+
+        return new ResponseEntity<>(resultList, HttpStatus.OK);
+    }
+
 
     public ResponseEntity<?> HoxyWatching(HttpServletRequest request) throws IOException {
 
