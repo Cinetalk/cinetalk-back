86c15475ac5bc78ceed1c6a7e09d4ea030ea44e6
TAEWOOKK <kto5294@gmail.com>
2024-11-04 03:56:52 +0900

    불필요한 파일삭제

--- src/main/java/com/back/cinetalk/didnotwhatchmovie/entity/DidNotWhatchMovieEntity.java
+++
@@ -1,4 +0,0 @@
-package com.back.cinetalk.didnotwhatchmovie.entity;
-
-public class DidNotWhatchMovieEntity {
-}
--- src/main/java/com/back/cinetalk/didnotwhatchmovie/repository/DidNotWhatchMovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.back.cinetalk.didnotwhatchmovie.repository;
-
-import com.back.cinetalk.didnotwhatchmovie.entity.DidNotWhatchMovieEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DidNotWhatchMovieRepository extends JpaRepository<DidNotWhatchMovieEntity, Long> {
-
-}
